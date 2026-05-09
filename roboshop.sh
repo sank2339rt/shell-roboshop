a4e74f57987afe4ae0f90d1e84df37cbd4cbbec6
sank2339 <sank2339rt@gmail.com>
2026-05-09 17:44:26 +0530

    roboshop updates

--- roboshop.sh
+++ roboshop.sh
@@ -1,36 +1,55 @@
 #!/bin/bash
 
-SG_ID="sg-047c75f124eff5499" # replace with your ID
+SG_ID="sg-047c75f124eff5499"
+SUBNET_ID="subnet-0b14763661b94a835"
 AMI_ID="ami-0220d79f3f480ecf5"
 ZONE_ID="Z05589412ZZNFD99WYGU4"
 DOMAIN_NAME="sank2339.online"
 
 for instance in $@
 do
+    echo "Creating instance: $instance"
+
     INSTANCE_ID=$( aws ec2 run-instances \
     --image-id $AMI_ID \
     --instance-type "t3.micro" \
     --security-group-ids $SG_ID \
+    --subnet-id $SUBNET_ID \
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text )
 
-    if [ $instance == "frontend" ]; then
-        IP=$(
-            aws ec2 describe-instances \
-            --instance-ids $INSTANCE_ID \
-            --query 'Reservations[].Instances[].PublicIpAddress' \
-            --output text
-        )
-        RECORD_NAME="$DOMAIN_NAME" # sank2339.online
+    # Validate instance creation
+    if [ -z "$INSTANCE_ID" ]; then
+        echo "Failed to create instance for $instance"
+        exit 1
+    fi
+
+    echo "Instance created with ID: $INSTANCE_ID"
+
+    # Wait until instance is running
+    aws ec2 wait instance-running --instance-ids $INSTANCE_ID
+
+    echo "Instance $instance is now running"
+
+    if [ "$instance" == "frontend" ]; then
+
+        IP=$( aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query 'Reservations[].Instances[].PublicIpAddress' \
+        --output text )
+
+        RECORD_NAME="$DOMAIN_NAME"
+
     else
-        IP=$(
-            aws ec2 describe-instances \
-            --instance-ids $INSTANCE_ID \
-            --query 'Reservations[].Instances[].PrivateIpAddress' \
-            --output text
-        )
-        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.sank2339.online
+
+        IP=$( aws ec2 describe-instances \
+        --instance-ids $INSTANCE_ID \
+        --query 'Reservations[].Instances[].PrivateIpAddress' \
+        --output text )
+
+        RECORD_NAME="$instance.$DOMAIN_NAME"
+
     fi
 
     echo "IP Address: $IP"
@@ -42,22 +61,22 @@ do
         "Comment": "Updating record",
         "Changes": [
             {
-            "Action": "UPSERT",
-            "ResourceRecordSet": {
-                "Name": "'$RECORD_NAME'",
-                "Type": "A",
-                "TTL": 1,
-                "ResourceRecords": [
-                {
-                    "Value": "'$IP'"
+                "Action": "UPSERT",
+                "ResourceRecordSet": {
+                    "Name": "'$RECORD_NAME'",
+                    "Type": "A",
+                    "TTL": 1,
+                    "ResourceRecords": [
+                        {
+                            "Value": "'$IP'"
+                        }
+                    ]
                 }
-                ]
-            }
             }
         ]
     }
     '
 
-    echo "record updated for $instance"
+    echo "DNS record updated for $instance"
 
 done
