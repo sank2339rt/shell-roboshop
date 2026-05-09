ddefc994e55ea110e33fd8d89781ca290737a435
sank2339 <sank2339rt@gmail.com>
2026-05-09 17:35:39 +0530

    roboshop updates

--- roboshop.sh
+++ roboshop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SG_ID="sg-076ec9ad23dab2b28" # replace with your ID
+SG_ID="sg-047c75f124eff5499" # replace with your ID
 AMI_ID="ami-0220d79f3f480ecf5"
-ZONE_ID="Z05013202FKF0ZL12WAOP"
-DOMAIN_NAME="daws88s.online"
+ZONE_ID="Z05589412ZZNFD99WYGU4"
+DOMAIN_NAME="sank2339.online"
 
 for instance in $@
 do
@@ -22,7 +22,7 @@ do
             --query 'Reservations[].Instances[].PublicIpAddress' \
             --output text
         )
-        RECORD_NAME="$DOMAIN_NAME" # daws88s.online
+        RECORD_NAME="$DOMAIN_NAME" # sank2339.online
     else
         IP=$(
             aws ec2 describe-instances \
@@ -30,7 +30,7 @@ do
             --query 'Reservations[].Instances[].PrivateIpAddress' \
             --output text
         )
-        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.daws88s.online
+        RECORD_NAME="$instance.$DOMAIN_NAME" # mongodb.sank2339.online
     fi
 
     echo "IP Address: $IP"
